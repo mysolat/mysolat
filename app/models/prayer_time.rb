093e85739df0ce1437bc1496f589cc3bc70a6d04
Mohd Khairi Mohd Adnan <khairi@labs.my>
2021-04-24 06:11:52 +0800

    Change expires

--- app/models/prayer_time.rb
+++ app/models/prayer_time.rb
@@ -3,13 +3,13 @@ module PrayerTime
   JAKIM_BASE_URL = "https://www.e-solat.gov.my/index.php?r=esolatApi/TakwimSolat&period=:period&zone=:zone"
 
   def self.daily(zone: 'SGR01')
-    Rails.cache.fetch("daily_#{zone}", expires_in: Time.current.end_of_day) do
+    Rails.cache.fetch("daily_#{zone}", expires_in: 1.day) do
       get_data('today', zone)
     end
   end
 
   def self.monthly(zone: 'SGR01')
-    Rails.cache.fetch("monthly_#{zone}", expires_in: Time.current.end_of_month) do
+    Rails.cache.fetch("monthly_#{zone}", expires_in: 1.month) do
       get_data('month', zone)
     end
   end
