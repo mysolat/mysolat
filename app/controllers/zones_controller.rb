cebb547fb5ddd64cfaa120e22d47c73cf1870a63
Mohd Khairi Mohd Adnan <khairi@labs.my>
2021-05-01 05:32:16 +0800

    Fix date

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -4,7 +4,8 @@ class ZonesController < ApplicationController
     @location =  @locations.find {|x| x['code'] == default_zone}
     prayer_times = PrayerTime.monthly(zone: default_zone)
     @monthly = prayer_times.fetch('prayerTime')
-    @today = @monthly.select { |p| p['date'] == Date.today.strftime("%d-%b-%Y") }.first
+    today = I18n.l Date.today, format:"%d-%b-%Y"
+    @today = @monthly.select { |p| p['date'] == today }.first
     @today.merge!({ 'bearing' => prayer_times['bearing'] })
   end
 
