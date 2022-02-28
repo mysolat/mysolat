4562c0538726e2b359d99400f2befa9e73a145a1
Mohd Khairi Mohd Adnan <khairi@labs.my>
2022-03-01 02:00:31 +0800

    Skip translate

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,10 +1,10 @@
 class ZonesController < ApplicationController
   def show
     @locations = Location.all
-    @location =  @locations.find {|x| x['code'] == default_zone}
+    @location =  @locations.find { |x| x['code'] == default_zone }
     prayer_times = PrayerTime.monthly(zone: default_zone)
     @monthly = prayer_times.fetch('prayerTime')
-    today = I18n.l Date.today, format:"%d-%b-%Y"
+    today = Date.today.strftime '%d-%b-%Y'
     @today = @monthly.select { |p| p['date'] == today }.first
     @today.merge!({ 'bearing' => prayer_times['bearing'] })
   end
