7c7055e9be988c92fcb3db63388df2b8d64ac643
Mohd Khairi Mohd Adnan <khairi@labs.my>
2022-09-05 12:40:41 +0800

    Rescue date leap

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -4,7 +4,7 @@ class ZonesController < ApplicationController
     @location =  @locations.find { |x| x['code'] == default_zone }
     prayer_times = PrayerTime.monthly(zone: default_zone)
     @monthly = prayer_times.fetch('prayerTime')
-    today = Date.today.strftime '%d-%b-%Y'
+    today = Date.current.strftime '%d-%b-%Y'
     @today = @monthly.select { |p| p['date'] == today }.first
     @today.merge!({ 'bearing' => prayer_times['bearing'] })
   end
