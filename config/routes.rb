fdef9d9c22c2996bb94731cb5d7e7381279dc6f2
Mohd Khairi <khairi@labs.my>
2024-10-09 14:22:26 +0000

    Upgrade rails 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  root to: "zones#show"
+
   namespace :api, defaults: { format: :json } do
     get "locations", to: "locations#index"
     get "daily/:zone", to: "prayer_times#daily"
@@ -11,9 +22,6 @@ Rails.application.routes.draw do
   resources :zones
 
   get "/pages/*id", to: "pages#show", as: :page
-  get "/manifest", to: "application#manifest", formats: [:json]
+  get "/manifest", to: "application#manifest", formats: [ :json ]
 
-  root to: "zones#show"
-
-  mount ActionCable.server => "/cable"
 end
